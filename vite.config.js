acb8f25514dedb8189dccece37ac6de6a80db478
kvtodev <kvtodev@outlook.com>
2025-10-13 10:37:56 +0800

    first build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     base: '/mdtools/',
+    build:{
+        outDir: 'docs',
+
+    },
     plugins: [
         react({
             babel: {
