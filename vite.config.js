64745a35329a648b92a683fbd618abee71387a79
kvtodev <kvtodev@outlook.com>
2025-10-27 10:38:48 +0800

    build
    # Conflicts:
    #	docs/assets/arc-CjhnlUoH.js
    #	docs/assets/blockDiagram-68f4deed-E65zAzGx.js
    #	docs/assets/c4Diagram-15b5d702-D5YzxWT2.js
    #	docs/assets/classDiagram-d40c83e7-BLJkZsVk.js
    #	docs/assets/classDiagram-v2-d5a6b087-CfoQCsjp.js
    #	docs/assets/createText-d213de94-Bx7jPZA4.js
    #	docs/assets/edges-332bd1c7-CPGU7SRL.js
    #	docs/assets/erDiagram-880f2ed8-BtQG8_5E.js
    #	docs/assets/flowDb-7c981674-Bh9_opiS.js
    #	docs/assets/flowDiagram-cbd28bf7-BYQhVVdv.js
    #	docs/assets/flowchart-elk-definition-36e2d292-B4NEECWo.js
    #	docs/assets/ganttDiagram-04f9e578-jDFhd71V.js
    #	docs/assets/gitGraphDiagram-21fc4d3e-_yuwsXUM.js
    #	docs/assets/graph-Ct_TdtRu.js
    #	docs/assets/index-6079d271-CYvrSqd7.js
    #	docs/assets/index-BFFceyw5.js
    #	docs/assets/index-BlyA1lBW.js
    #	docs/assets/index-C6vllite.js
    #	docs/assets/index-CBnyIPK4.js
    #	docs/assets/index-CU4grVD8.js
    #	docs/assets/index-CoVm5sS0.js
    #	docs/assets/index-Cs53_qND.js
    #	docs/assets/index-CxIho9Ew.js
    #	docs/assets/index-Cz6aZU6c.js
    #	docs/assets/index-D1jcaeAY.js
    #	docs/assets/index-DJKZGNsh.js
    #	docs/assets/index-DRHQXc_H.js
    #	docs/assets/index-DSBWdv79.js
    #	docs/assets/index-PdEE_W-x.js
    #	docs/assets/index-asFbuzy8.js
    #	docs/assets/index-kOmuybhd.js
    #	docs/assets/index-rrmyqE8h.js
    #	docs/assets/index.es-C3kVyIvb.js
    #	docs/assets/infoDiagram-4a4f5b27-DzTizU3_.js
    #	docs/assets/journeyDiagram-29694f62-rZRYRK5_.js
    #	docs/assets/layout-Cjqva3l8.js
    #	docs/assets/line-DRzNZoGh.js
    #	docs/assets/linear-kRFZnDld.js
    #	docs/assets/mindmap-definition-ac74a2e8-CCp6ubg2.js
    #	docs/assets/pieDiagram-421022e6-Dr87KQOF.js
    #	docs/assets/quadrantDiagram-0957ecba-CIXiVc1Z.js
    #	docs/assets/requirementDiagram-23d650b8-COM41WQ1.js
    #	docs/assets/sankeyDiagram-23345273-Z5TKRrr8.js
    #	docs/assets/sequenceDiagram-17ac3bff-DeaTjzjg.js
    #	docs/assets/stateDiagram-9c5f0230-Zz7tor7s.js
    #	docs/assets/stateDiagram-v2-51a3dcff-DeTImk6J.js
    #	docs/assets/styles-2ab5d517-BHaqPkIb.js
    #	docs/assets/styles-5f03d8d2-j5Vx9OEM.js
    #	docs/assets/styles-edf9a4b0-BERIYrmz.js
    #	docs/assets/svgDrawCommon-3ba9043b-CCov-OOJ.js
    #	docs/assets/timeline-definition-7e6b55e7-B1s0GZEI.js
    #	docs/assets/xychartDiagram-b6496bcd-DxXb-MVm.js
    #	docs/index.html

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/mdtools/',
     build:{
-        outDir: 'docs',
+        outDir: 'dist',
 
     },
     plugins: [
